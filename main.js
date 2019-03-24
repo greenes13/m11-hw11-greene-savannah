07034fea8b0da6ba1511b1d827ecac5af9a50db2
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-03-24 08:15:38 -0500

    Add functionality with for loop using let

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ const myBoxes = document.getElementsByClassName('block');
 //The Functionality
 function loopThrough() {
 
+  for (let i = 0; i < this.textContent.length; i ++) {
+    
 };
 
 //The Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
-  myBoxes[i].addEventListener('click', loopthrough)
-}
+  myBoxes[i].addEventListener('click', loopThrough);
+};
+
+
+// ES6 syntax uses const and let instead of var
+// function declaration in global scope
+//"let" defines initialization
+// event listener listens for 'click' and runs function 'loopthrough'
