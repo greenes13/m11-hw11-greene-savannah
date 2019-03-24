42ae2b34f3408a6b803e5a937051825d37779db3
Savannah  Greene <savannahgreene@Savannahs-MacBook-Pro-2.local>
2019-03-24 08:31:02 -0500

    Add on and off functionality

--- main.js
+++ main.js
@@ -6,19 +6,12 @@ function loopThrough() {
 
   const changeColor = () => this.classList.toggle('red');
 
-  for (let i = 0; i < this.textContent.length; i ++) {
-
+  for (let i = 0; i < this.textContent.length * 2; i ++) {
+    setTimeout(changeColor, i * 1000)
+  };
 };
 
 //The Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
 };
-
-
-//NOTES
-// ES6 syntax uses const and let instead of var
-// function declaration in global scope
-//"let" defines initialization
-// event listener listens for 'click' and runs function 'loopthrough'
-//function defined using const and for loop using let initiating the fucntion
